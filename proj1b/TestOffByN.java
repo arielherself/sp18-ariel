57a24363bbd40b7589b01a28ddcfa9bcb6ca7772
arielherself <ariel@arielherself.xyz>
2023-08-17 19:24:02 +0800

    proj1b: rename TestOffByN.testOffByN

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 
 public class TestOffByN {
     @Test
-    public void testOffByN() {
+    public void testEqualChars() {
         OffByN offBy5 = new OffByN(5);
         assertTrue(offBy5.equalChars('a', 'f'));
         assertTrue(offBy5.equalChars('f', 'a'));
