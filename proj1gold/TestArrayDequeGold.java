1e5eb9542a4bfcee6d8ba92f63806d0c5aa0a247
arielherself <ariel@arielherself.xyz>
2023-08-19 23:53:08 +0800

    pro1gold: refactor TestArrayDequeGold.testArrayDeque to show a callback history

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,8 +1,44 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import java.util.Objects;
+import static org.junit.Assert.assertEquals;
+
+record funcCallbackRecord(String functionName, int argument) {
+    @Override
+    public String toString() {
+        if (argument == -1) {
+            return String.format("%s()", functionName);
+        } else {
+            return String.format("%s(%d)", functionName, argument);
+        }
+    }
+}
+
+class Stack<T> {
+    private record ListNode<T> (T item, ListNode<T> prev) {}
+
+    ListNode<T> last;
+
+    public void push(T i) {
+        last = new ListNode<>(i, last);
+    }
+
+    public T pop() {
+        T result = last.item();
+        last = last.prev();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode<T> ref = last;
+        while (ref != null) {
+            sb.append(ref.item()).append("\n");
+            ref = ref.prev();
+        }
+        return sb.toString();
+    }
+}
 
 public class TestArrayDequeGold {
     @Test
@@ -11,6 +47,7 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> provenCorrect = new ArrayDequeSolution<>();
 
         final int rounds = 1000;
+        Stack<funcCallbackRecord> history = new Stack<>();
         int choice, argument, dequeLen = 0;
         for (int i = 0; i < rounds; ++i) {
             argument = StdRandom.uniform(10000);
@@ -18,21 +55,29 @@ public class TestArrayDequeGold {
                 choice = StdRandom.uniform(4);
                 switch (choice) {
                     case 0 -> {
+                        history.push(new funcCallbackRecord("addFirst", argument));
                         toTest.addFirst(argument);
                         provenCorrect.addFirst(argument);
                         ++dequeLen;
                     }
                     case 1 -> {
+                        history.push(new funcCallbackRecord("addLast", argument));
                         toTest.addLast(argument);
                         provenCorrect.addLast(argument);
                         ++dequeLen;
                     }
                     case 2 -> {
-                        assertTrue(Objects.equals(provenCorrect.removeFirst(), toTest.removeFirst()));
+                        var a = provenCorrect.removeFirst();
+                        history.push(new funcCallbackRecord("removeFirst", -1));
+                        var b = toTest.removeFirst();
+                        assertEquals(history.toString(), a, b);
                         --dequeLen;
                     }
                     case 3 -> {
-                        assertTrue(Objects.equals(provenCorrect.removeLast(), toTest.removeLast()));
+                        var a = provenCorrect.removeLast();
+                        history.push(new funcCallbackRecord("removeLast", -1));
+                        var b = toTest.removeLast();
+                        assertEquals(history.toString(), a, b);
                         --dequeLen;
                     }
                     default -> {
@@ -42,11 +87,13 @@ public class TestArrayDequeGold {
                 choice = Boolean.compare(StdRandom.bernoulli(), false);
                 switch (choice) {
                     case 0 -> {
+                        history.push(new funcCallbackRecord("addFirst", argument));
                         toTest.addFirst(argument);
                         provenCorrect.addFirst(argument);
                         ++dequeLen;
                     }
                     case 1 -> {
+                        history.push(new funcCallbackRecord("addLast", argument));
                         toTest.addLast(argument);
                         provenCorrect.addLast(argument);
                         ++dequeLen;
