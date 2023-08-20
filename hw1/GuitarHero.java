9dbcf3c170a70aec038875130d85a677051cf303
arielherself <ariel@arielherself.xyz>
2023-08-20 12:17:56 +0800

    hw1: style refinement

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -2,12 +2,12 @@ import synthesizer.GuitarString;
 
 public class GuitarHero {
     private static final double CONCERT_A = 440.0;
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
-    private static final int keysCount = keyboard.length();
-    private final GuitarString[] strings = new GuitarString[keysCount];
+    private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final int KEYS_COUNT = KEYBOARD.length();
+    private final GuitarString[] strings = new GuitarString[KEYS_COUNT];
 
     public GuitarHero() {
-        for (int i = 0; i < keysCount; ++i) {
+        for (int i = 0; i < KEYS_COUNT; ++i) {
             strings[i] = new GuitarString(CONCERT_A * Math.pow(2, (double) (i - 24) / 12));
         }
     }
@@ -17,14 +17,14 @@ public class GuitarHero {
     }
 
     public void ticAll() {
-        for (int i = 0; i < keysCount; ++i) {
+        for (int i = 0; i < KEYS_COUNT; ++i) {
             strings[i].tic();
         }
     }
 
     public double sampleAll() {
         double result = 0;
-        for (int i = 0; i < keysCount; ++i) {
+        for (int i = 0; i < KEYS_COUNT; ++i) {
             result += strings[i].sample();
         }
         return result;
@@ -37,7 +37,7 @@ public class GuitarHero {
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
                 try {
-                    guitar.pluck(keyboard.indexOf(key));
+                    guitar.pluck(KEYBOARD.indexOf(key));
                 } catch (ArrayIndexOutOfBoundsException e) {
                     e.printStackTrace();
                 }
