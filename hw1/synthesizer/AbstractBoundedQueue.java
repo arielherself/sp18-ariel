542a0d25fa774ea6f9d2bcf986b95062259ec4b1
arielherself <ariel@arielherself.xyz>
2023-08-20 10:20:53 +0800

    hw1: add comments for AbstractBoundedQueue

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,6 +1,8 @@
 package synthesizer;
 
 public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    /* The number of stored elements in the array. */
     protected int fillCount;
+    /* The maximum of fillCount. */
     protected int capacity;
 }
