561d268a3a4dc513a06864324f42a268fce071bd
arielherself <ariel@arielherself.xyz>
2023-08-31 22:35:52 +0800

    lab11: implement MazeCycles

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  *  @author Josh Hug
  */
@@ -9,14 +11,34 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private final WeightedQuickUnionUF qu = new WeightedQuickUnionUF(maze.V());
 
     public MazeCycles(Maze m) {
         super(m);
     }
 
+    public void dfs(int v) {
+        marked[v] = true; // preorder dfs
+        for (int child : maze.adj(v)) {
+            edgeTo[child] = v;
+            if (marked[child]) {
+                if (qu.connected(child, v)) {
+                    announce();
+                    return ;
+                } else {
+                    qu.union(child, v);
+                }
+            } else {
+                dfs(child);
+            }
+        }
+        announce();
+    }
+
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0);
     }
 
     // Helper methods go here
